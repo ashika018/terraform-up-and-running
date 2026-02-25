81ce9eabfabf251e818071914f26452d19e936e7
ashika018 <ha.ashikawa18@gmail.com>
2026-02-26 00:27:23 +0900

    [feat] SG付与と変数の導入

--- terraform/00-preface/hello-world/main.tf
+++ terraform/00-preface/hello-world/main.tf
@@ -4,10 +4,40 @@ provider "aws" {
 }
 
 resource "aws_instance" "example"{
-  ami = "ami-088103e734f7e0529"
+  ami = "ami-0f65fc8c24ec8d2a1"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [ aws_security_group.instance.id ]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+  user_data_replace_on_change = true
 
   tags = {
     Name = "terraform.example"
+  }
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type = number
+  default = 8080
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port = var.server_port
+    to_port = var.server_port
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
